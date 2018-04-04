ef4ac7c4af3d01e11bf8a38b80c20bcee9f409b4
Aly Badran <aly2cool@hotmail.com>
2018-04-03 20:57:15 -0400

    displays welcome message on user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/user_profile', to: "user_profile#index"
   get '/', to: "products#index"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
